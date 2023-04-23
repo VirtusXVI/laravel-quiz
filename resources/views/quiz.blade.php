c4457576d3c351a3a071b787152e2cf6a3751fac
VirtusXVI <manuel.aveta2001@gmail.com>
2023-04-23 15:43:14 +0200

    first commit

---
+++ resources/views/quiz.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/app.css">
+    <title>Quiz</title>
+</head>
+<body>
+    <div class="flex wrapper">
+        <div class="first flex justify-center items-center">
+            <div class="max-w-sm">
+                <h1 class="text-3xl font-bold">
+                    Quale paese in Europa detiene il record per il maggiore consumo di acqua in bottiglia?
+                </h1>
+                <p class="mt-7 text-xl">
+                    Sottotitolo eventuale, non più lungo di così: 
+                    Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut 
+                    enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi
+                </p>
+            </div>
+        </div>
+        <div class="flex justify-center items-center">
+            ciao2
+        </div>
+    </div>
+</body>
+</html>
