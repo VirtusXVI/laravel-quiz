cdbeda6b72d0318ed7bb6f550255561d2b61a2da
VirtusXVI <manuel.aveta2001@gmail.com>
2023-04-23 17:49:35 +0200

    completed without vue

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -8,7 +8,7 @@ class AnswerController extends Controller
 {
     public function checkAnswer(Request $request){
         $answer = $request->all();
-        if($answer['answer'] == 1){
+        if($answer['answer'] == 3){
             return redirect()->back()->with('success', 'your message,here');
         }else{
             return redirect()->back()->with('error', 'your message,here');
--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -26,36 +26,72 @@
             </div>
             <div class="flex justify-center items-center">
                 @if (session('success'))
-                    <div class="alert alert-success">
-                        successo
+                    <div class="check">
+                        <h2 class="text-3xl font-bold text-green-400">Giusto</h2>
+                        <div>
+                            <h2 class="text-3xl font-bold text-gray-400 my-10">
+                                Italia
+                            </h2>
+                            <p class="text-gray-400 ">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione molestias animi aliquam deleniti impedit facere ipsum suscipit tempora sequi, quo, ullam nobis blanditiis debitis, harum nostrum ipsa porro ducimus.
+                            </p>
+                        </div>
                     </div>
                 @endif
                 @if (session('error'))
-                    <div class="alert alert-success">
-                        errore
+                    <div class="check">
+                        <h2 class="text-3xl font-bold text-red-400">Purtroppo no, la risposta corretta è...</h2>
+                        <div>
+                            <h2 class="text-3xl font-bold text-gray-400 my-10">
+                                Italia
+                            </h2>
+                            <p class="text-gray-400 ">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione molestias animi aliquam deleniti impedit facere ipsum suscipit tempora sequi, quo, ullam nobis blanditiis debitis, harum nostrum ipsa porro ducimus.
+                            </p>
+                        </div>
                     </div>
                 @endif
                 @if(!session('success') && !session('error'))
                     <form method="post" class="flex flex-col" action="{{ url('check-answer') }}">
                         @csrf
                         <div class="flex flex-col gap-6 mb-10">
-                            <label for="answer_1" class="font-bold answer">
+                            <label onclick="select()" id="first_answer" for="answer_1" class="font-bold answer">
                                 Francia
                                 <input type="radio" id="answer_1" class="answer_radio" name="answer" value="1">
                             </label>
-                            <label for="answer_2" class="font-bold answer">
+                            <label onclick="select()" id="second_answer" for="answer_2" class="font-bold answer">
                                 Germania
                                 <input type="radio" id="answer_2" class="answer_radio" name="answer" value="2">
                             </label>
-                            <label for="answer_3" class="font-bold answer">
+                            <label onclick="select()" id="third_answer" for="answer_3" class="font-bold answer">
                                 Italia
                                 <input type="radio" id="answer_3" class="answer_radio" name="answer" value="3">
                             </label>
                         </div>
-                        <button class="submit_button self-end font-bold">Avanti</button>
+                        <button class="submit_button self-end font-bold disabled" disabled>Avanti</button>
                     </form>
                 @endif
             </div>
         </div>
     </body>
+
+    <script>
+        function select(){
+            selected_item = document.querySelectorAll('.answer_radio');
+            answer_boxes = document.querySelectorAll('.answer');
+            answer_boxes.forEach(el => {
+                el.classList.remove('selected');
+            });
+            selected_item.forEach(el => {
+                if(el.checked == true){
+                    selected_answer = document.querySelector('#' + el.labels[0].id);
+                    selected_answer.classList.add('selected');
+                    submit_button = document.querySelector('.submit_button');
+                    submit_button.disabled = false;
+                    submit_button.classList.remove('disabled');
+                }
+            });
+        }
+    </script>
+
 </html>
